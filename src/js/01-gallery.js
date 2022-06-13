b85a0fc2eca0b73750805e5caa2238b1b2a407e8
Iryna Vakilova <iravakilova@gmail.com>
2022-06-13 12:55:56 +0300

    gallery grid

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryItem = galleryItems
+        `<div class = 'gallery__item'>
+            <a class = 'gallery__link' href = '${original}'>
+                <img class = 'gallery__image'
+                src = ${preview}
+                data-source = ${original}
+                alt = ${description}
+                />
+            </a>
+        </div>`)
+    .join(''); 
+gallery.insertAdjacentHTML('beforeend', galleryItem);
