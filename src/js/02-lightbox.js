baad02ef7143cb965430933cc690dc1a783b485d
Iryna Vakilova <iravakilova@gmail.com>
2022-06-14 17:46:52 +0300

    task-2 done

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryItem = galleryItems
+    .map(({ preview, original, description }) =>
+        `<li><a class = 'gallery__item' href = '${original}'>
+            <img class = 'gallery__image'
+            src = ${preview}
+            alt = ${description}
+            />
+        </a></li>`)
+    .join(''); 
+gallery.insertAdjacentHTML('beforeend', galleryItem);
+
+gallery.addEventListener('click', clickImage);
+function clickImage(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const lightbox = new SimpleLightbox('.gallery a', {
+        event,
+        captionSelector: 'img',
+        captionsData: 'alt',
+        captionDelay: 250,
+        });
+};
